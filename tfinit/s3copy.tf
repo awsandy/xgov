307ce0da8f94968c29b15410a100720caee9c3a5
awsandy <awsandy@amazon.co.uk>
2023-02-21 19:07:46 +0000

    init

--- tfinit/s3copy.tf
+++ tfinit/s3copy.tf
@@ -1,20 +1,9 @@
-resource "aws_s3_object_copy" "customers1" {
+resource "aws_s3_object_copy" "customers" {
   bucket = aws_s3_bucket.xgov-data.id
-  key    = "raw-data/customers1.csv"
-  source = "event-engine-eu-west-1/xgovdata/customers1.csv"
+  key    = "raw-data/customers.csv"
+  source = format("event-engine-eu-west-1/xgovdata/customers%s.csv",var.team_number)
 }
 
-resource "aws_s3_object_copy" "customers2" {
-  bucket = aws_s3_bucket.xgov-data.id
-  key    = "raw-data/customers2.csv"
-  source = "event-engine-eu-west-1/xgovdata/customers2.csv"
-}
-
-resource "aws_s3_object_copy" "customers3" {
-  bucket = aws_s3_bucket.xgov-data.id
-  key    = "raw-data/customers3.csv"
-  source = "event-engine-eu-west-1/xgovdata/customers3.csv"
-}
 
 resource "aws_s3_object_copy" "products" {
   bucket = aws_s3_bucket.xgov-data.id
