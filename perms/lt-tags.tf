a38bb520c49188c1d4767f7490168549522e4daf
awsandy <awsandy@amazon.co.uk>
2023-02-22 21:38:43 +0000

    up

--- perms/lt-tags.tf
+++ perms/lt-tags.tf
@@ -1,5 +1,4 @@
 resource "aws_lakeformation_lf_tag" "example" {
-  depends_on=[aws_lakeformation_permissions.xgov-db-admin]
   key    = "sensitivity"
   values = ["public", "private", "confidential"]
 }
