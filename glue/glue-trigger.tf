0b0d6c880f4c06c5492a0521e1e3f4f34654cb6b
awsandy <awsandy@amazon.co.uk>
2023-02-22 20:51:00 +0000

    up

--- glue/glue-trigger.tf
+++ glue/glue-trigger.tf
@@ -1,7 +1,7 @@
 resource "aws_glue_trigger" "example" {
   name       = "example"
   type       = "ON_DEMAND"
-  depends_on = [aws_glue_catalog_database.xgov.]
+  depends_on = [aws_glue_catalog_database.xgov]
 
 
   actions {
