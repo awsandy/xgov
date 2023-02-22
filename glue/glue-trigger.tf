8c291fb8cd61e1b7988e98eb09d4cc2c2293ee20
awsandy <awsandy@amazon.co.uk>
2023-02-22 21:02:56 +0000

    up

--- glue/glue-trigger.tf
+++ glue/glue-trigger.tf
@@ -2,7 +2,7 @@ resource "aws_glue_trigger" "example" {
   name       = "example"
   type       = "ON_DEMAND"
   depends_on = [aws_glue_catalog_database.xgov]
-  role=data.aws_iam_role.LF-GlueServiceRole.arn
+
 
 
   actions {
