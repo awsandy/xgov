cb8228522333a6a5bfd9cd4c180f6a2c458799d9
awsandy <awsandy@amazon.co.uk>
2023-02-22 21:00:23 +0000

    up

--- glue/lf-permissions-glue.tf
+++ glue/lf-permissions-glue.tf
@@ -1,6 +1,7 @@
 resource "aws_lakeformation_permissions" "xgov-db-glue" {
   principal   = data.aws_iam_role.LF-GlueServiceRole.arn
   permissions = ["CREATE_TABLE", "ALTER", "DROP"]
+  permissions_with_grant_option=["CREATE_TABLE", "ALTER", "DROP"]
   depends_on = [
     aws_glue_catalog_database.xgov
   ]
