ded2f9e0c850f2c9d10d1b270650b9dc47ab4cb6
awsandy <awsandy@amazon.co.uk>
2023-02-22 17:33:28 +0000

    up

---
+++ glue/glue-db.tf
@@ -0,0 +1,16 @@
+# Error: creating Glue Catalog Database (xgov): 
+# AccessDeniedException: Insufficient Lake Formation permission(s): Required Create Database on Catalog
+resource "aws_glue_catalog_database" "xgov" {
+  name = "xgov"
+}
+
+
+
+
+
+
+
+
+
+
+
