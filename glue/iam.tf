1ec81ca53d0aeb394d62389dc2a0352f71fedd70
awsandy <awsandy@amazon.co.uk>
2023-02-22 17:39:00 +0000

    up

--- glue/iam.tf
+++ glue/iam.tf
@@ -1,11 +1,9 @@
 # this role comes from CloudFormatio
 data "aws_iam_role" "LF-GlueServiceRole" {
-    depends_on=[aws_cloudformation_stack.lf-users]
     name = "LF-GlueServiceRole"
 }
 
 data "aws_iam_role" "WSParticipantRole" {
-    depends_on=[aws_cloudformation_stack.lf-users]
     name = "WSParticipantRole"
 }
 
