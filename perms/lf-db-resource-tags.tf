dd8c039afba46551acf9660300d91166bbb5edf8
awsandy <awsandy@amazon.co.uk>
2023-02-23 10:54:04 +0000

    up

--- perms/lf-db-resource-tags.tf
+++ perms/lf-db-resource-tags.tf
@@ -1,16 +1,6 @@
-resource "aws_lakeformation_resource_lf_tags" "example" {
+resource "aws_lakeformation_resource_lf_tags" "db-xgov" {
   database {
     name = "xgov"
-  }
-
-  lf_tag {
-    key   = "sensitivity"
-    value = "public"
-  }
-
-  lf_tag {
-    key   = "sensitivity"
-    value = "private"
   }
 
   lf_tag {
@@ -18,3 +8,70 @@ resource "aws_lakeformation_resource_lf_tags" "example" {
     value = "confidential"
   }
 }
+
+
+resource "aws_lakeformation_resource_lf_tags" "tables-xgov" {
+  table {
+    database_name=aws_lakeformation_resource_lf_tags.db-xgov.database
+    wildcard=true
+  }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "confidential"
+  }
+}
+
+resource "aws_lakeformation_resource_lf_tags" "table-customer-public" {
+  table {
+    database_name=aws_lakeformation_resource_lf_tags.db-xgov.database
+    name="customers"
+    column_names=["Customer_ID"]
+  }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "public"
+  }
+}
+
+
+resource "aws_lakeformation_resource_lf_tags" "table-customer-public" {
+  table {
+    database_name=aws_lakeformation_resource_lf_tags.db-xgov.database
+    name="customers"
+    column_names=["customer_id"]
+  }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "public"
+  }
+}
+
+resource "aws_lakeformation_resource_lf_tags" "table-customer-private" {
+  table {
+    database_name=aws_lakeformation_resource_lf_tags.db-xgov.database
+    name="customers"
+    column_names=["prefix","first_name","middle_name","last_name","suffix","gender"]
+  }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "public"
+  }
+}
+
+resource "aws_lakeformation_resource_lf_tags" "table-customer-confidential" {
+  table {
+    database_name=aws_lakeformation_resource_lf_tags.db-xgov.database
+    name="customers"
+    column_names=["dob","address"]
+  }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "public"
+  }
+}
+
