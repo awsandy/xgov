d1ed12b753bc7dee4233e08a96b19417fba0db00
awsandy <awsandy@amazon.co.uk>
2023-02-23 08:39:11 +0000

    up

--- tfinit/glue-crawler.tf
+++ tfinit/glue-crawler.tf
@@ -11,16 +11,10 @@ resource "aws_glue_crawler" "xgov" {
 }
 
 # nope
-#resource "aws_glue_crawler" "xgov2" {
-#  database_name = "xgov"
-#  name          = "xgov2"
-#  # this role comes from CloudFormation
+
 #  role          = data.aws_iam_role.lf-admin.arn#
 
-#  s3_target {
-#    path = "s3://${aws_s3_bucket.xgov-data.bucket}/raw-data"
-#  }
-#}
+
 
 
 
--- tfinit/s3copy.tf
+++ tfinit/s3copy.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object_copy" "customers" {
   bucket = aws_s3_bucket.xgov-data.id
-  key    = "raw-data/customers.csv"
+  key    = "raw-data/customers/customers.csv"
   source = format("event-engine-eu-west-1/xgovdata/customers%s.csv",var.team_number)
 }
 
@@ -8,11 +8,11 @@ resource "aws_s3_object_copy" "customers" {
 resource "aws_s3_object_copy" "products" {
   bucket = aws_s3_bucket.xgov-data.id
   key    = "raw-data/products.csv"
-  source = "event-engine-eu-west-1/xgovdata/products.csv"
+  source = "event-engine-eu-west-1/xgovdata/products/products.csv"
 }
 
 resource "aws_s3_object_copy" "sales" {
   bucket = aws_s3_bucket.xgov-data.id
   key    = "raw-data/sales.csv"
-  source = "event-engine-eu-west-1/xgovdata/sales.csv"
+  source = "event-engine-eu-west-1/xgovdata/sales/sales.csv"
 }
