6437f90e80df767721ad2814b2a061e860003d9d
awsandy <awsandy@amazon.co.uk>
2023-02-23 11:46:42 +0000

    up

--- perms/lf-permissions-table.tf
+++ perms/lf-permissions-table.tf
@@ -2,12 +2,13 @@ resource "aws_lakeformation_permissions" "test" {
   principal                     = data.aws_iam_role.lf-admin.arn
   permissions                   = ["ALL", "ALTER", "DELETE", "DESCRIBE", "DROP", "INSERT", "SELECT"]
   permissions_with_grant_option = ["ALL", "ALTER", "DELETE", "DESCRIBE", "DROP", "INSERT", "SELECT"]
-  table {
+
+  lf_tag_policy {
+    resource_type = "TABLE"
+      table {
     database_name = "xgov"
     name      = "customers"
   }
-  lf_tag_policy {
-    resource_type = "TABLE"
 
     expression {
       key    = aws_lakeformation_lf_tag.tags-xgov.key
